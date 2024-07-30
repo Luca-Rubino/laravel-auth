f6bdd5bf8903a4d043ae8d1feafbb7b3e47704e8
Luca-Rubino <lucasrubino92@gmail.com>
2024-07-30 18:20:11 +0200

    Insert route in tag a in navbar

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -7,11 +7,17 @@
                     </a>
                 </li>
                 <li>
-                    <a href="/chi_siamo">Chi siamo</a>
+                    <a href="/Chi_siamo">chi siamo</a>
                 </li>
-                <li>item</li>
-                <li>item</li>
-                <li>item</li>
+                <li>
+                    <a href="/Exercise">exercise</a>
+                </li>
+                <li>
+                    <a href="/Blog">blog</a>
+                </li>
+                <li>
+                    <a href="/Contatti">contatti</a>
+                </li>
             </ul>
     </section>
 @endsection
