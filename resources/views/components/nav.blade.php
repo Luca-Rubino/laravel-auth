9ec9df7f4785b07343e7d952fe18e3db28d726eb
Luca-Rubino <lucasrubino92@gmail.com>
2024-07-30 13:16:24 +0200

    Insert element in nav

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,8 +1,14 @@
 @section('navbar')
     <section id="navbar" class="container-fluid">
             <ul>
-                <li><img src="{{ Vite::asset("resources/img/boolean.ico") }}" alt="{{ 'logo' }}"></li>
-                <li>item</li>
+                <li>
+                    <a href="/">
+                        <img src="{{ Vite::asset("resources/img/boolean.ico") }}" alt="{{ 'logo' }}">
+                    </a>
+                </li>
+                <li>
+                    <a href="/chi_siamo">Chi siamo</a>
+                </li>
                 <li>item</li>
                 <li>item</li>
                 <li>item</li>
